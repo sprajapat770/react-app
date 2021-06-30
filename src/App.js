213f3412daa653f6105962109d5dc0417b99c1b8
Suraj Prajapat <sprajapat770@gmail.com>
2021-06-30 19:42:23 +0530

    tic toe game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Suraj App</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+
+class Board extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        }
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+  renderSquare(i){
+    return <Square value={this.state.squares[i]}
+                   onClick={() => this.handleClick(i)}/>;
+  }
+  render() {
+    const title = 'tic-tac-toe Game';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    return (
+        <div>
+          <div className="title">{title}</div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+    );
+  }
 }
 
-export default App;
+
+class Game extends React.Component {
+  render() {
+    return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+    );
+  }
+}
+
+class Square extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      value:null,
+    }
+  }
+  render() {
+    return (
+        <button className="square" onClick={() => this.props.onClick() }>
+          {this.props.value}
+        </button>
+    );
+  }
+}
+
+export default Game;
